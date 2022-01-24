6a77786360a7b557ecad503b55c0e3eb0dff285d
Erik Fotta <erik.fotta55@gmail.com>
2022-01-23 23:55:55 -0500

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Erik Fotta on 1/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
