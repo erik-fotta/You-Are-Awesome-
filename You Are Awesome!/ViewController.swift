e7d07ed7235a10751eb7a52157ea5618bf091ebc
Erik Fotta <erik.fotta55@gmail.com>
2022-02-07 13:06:32 -0500

    Sounds

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,7 @@ class ViewController: UIViewController {
     var imageNumber = -1
     var messageNumber = -1
     let totalNumberOfImages = 9
+    var audioPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,6 +43,17 @@ class ViewController: UIViewController {
         } while imageNumber == newImageNumber
         imageNumber = newImageNumber
         imageView.image = UIImage(named: "image1")
+        
+        if let sound = NSDataAsset(name: "sound0") {
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("ERROR: Couldnt not initialize audioplayer")
+            }
+        } else {
+            print("ERROR: Couldnt read sound file")
+        }
     }
 }
         
